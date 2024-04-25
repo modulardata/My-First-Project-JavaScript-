3b84c914aa97a02d59262f332442dc1930475905
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-04-25 07:14:58 +0200

    Initial setup Stage 1/3 done

---
+++ My First Project (JavaScript)/task/task.js
@@ -0,0 +1,18 @@
+const input = require('sync-input');
+
+// We have imported the 'sync-input' package for you.
+// This package allows you to get user input.
+// Like so:
+// let name = input("Type your name: ");
+// let age = Number(input("Type your age: "));
+// You will need it in later stages.
+
+
+
+console.log(`Prices:
+Bubblegum: $2
+Toffee: $0.2
+Ice cream: $5
+Milk chocolate: $4
+Doughnut: $2.5
+Pancake: $3.2`);
---
+++ My First Project (JavaScript)/task/tests.py
@@ -0,0 +1,5 @@
+from test.tests import PrintFirstProject
+
+
+if __name__ == '__main__':
+    PrintFirstProject('task').run_tests()
